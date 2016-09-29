6704e3468d7a8da9bcd4b65789f117b05ecceb54
Mirko Ferraro <mirkoferraro@gmail.com>
2016-09-29 18:28:31 +0200

    ignore default post count limit

---
+++ public/app/mu-plugins/functions/wp-query.php
@@ -0,0 +1,8 @@
+<?php
+
+add_action('pre_get_posts', 'ignore_post_limit', 100, 1 );
+function ignore_post_limit( &$query ) {
+  if ( ! isset( $query->query_vars['posts_per_page'] ) ) {
+    $query->query_vars['posts_per_page'] = -1;
+  }
+}
