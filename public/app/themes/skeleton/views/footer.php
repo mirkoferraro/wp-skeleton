188275b758aa5b49e79ca3ba8342c730d5f84fdc
Mirko Ferraro <mirkoferraro@gmail.com>
2016-10-14 19:24:45 +0200

    update footer

--- public/app/themes/skeleton/views/footer.php
+++ public/app/themes/skeleton/views/footer.php
@@ -1,8 +1,6 @@
 <?php check_directly_access(); ?>
 
 <footer class="footer" role="contentinfo">
-	<p class="copyright">
-		&copy; <?= date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?= _t('Powered by'); ?>
-		<a href="//skeleton.com" title="Skeleton">Skeleton</a>.
+		<?= _t('Powered by'); ?> <a href="//skeleton.com" title="Skeleton">Skeleton</a>.
 	</p>
 </footer>
