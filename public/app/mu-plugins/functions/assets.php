bf0092ed919a0718e0fe4bb2409626f8b79bb4f5
Mirko Ferraro <mirkoferraro@gmail.com>
2016-11-06 19:01:07 +0100

    minor update

--- public/app/mu-plugins/functions/assets.php
+++ public/app/mu-plugins/functions/assets.php
@@ -43,7 +43,7 @@ add_action( 'wp_footer', 'print_svg_sprite', 0 );
 function print_svg_sprite() {
 	?>
 	<div style="display: none;">
-	<?php include WP_DIR . '/assets/img/svg-sprite.svg'; ?>
+	<?php include assets_path('img') . '/svg-sprite.svg'; ?>
 	</div>
 	<?php
 }
@@ -51,7 +51,7 @@ function print_svg_sprite() {
 add_action( 'wp_footer', 'load_async_css', 1000 );
 function load_async_css() {
 	global $async_styles;
-	$libpath = WP_DIR . '/assets/js/lib/loadcss.min.js';
+	$libpath = assets_path('js') . '/lib/loadcss.min.js';
 	if ( is_array( $async_styles ) && count( $async_styles ) && file_exists( $libpath ) ) : ?>
 	<script type="text/javascript">
 		<?php
@@ -68,7 +68,7 @@ add_action( 'wp_footer', 'font_loader', 1000 );
 function font_loader() {
 	?>
 	<script type="text/javascript">
-	<?php include WP_DIR . '/assets/js/lib/fontloader.min.js'; ?>
+	<?php include assets_path('js') . '/lib/fontloader.min.js'; ?>
 	fontloader.onload({
 		'Lato': [400,300,700],
 	});
