23da2c50a55524f48070a721f21c7f4837b1a507
Mirko Ferraro <mirkoferraro@gmail.com>
2016-11-06 18:14:32 +0100

    svg-srpite visibility

--- public/app/mu-plugins/functions/assets.php
+++ public/app/mu-plugins/functions/assets.php
@@ -2,23 +2,23 @@
 check_directly_access();
 
 function assets_url( $type = null ) {
-    $url = home_url() . '/assets';
+	$url = home_url() . '/assets';
 
-    if ( $type != null && in_array( $type, array( 'css', 'favicons', 'fonts', 'img', 'js' ) ) ) {
-        $url .= '/' . $type;
-    }
+	if ( $type != null && in_array( $type, array( 'css', 'favicons', 'fonts', 'img', 'js' ) ) ) {
+		$url .= '/' . $type;
+	}
 
-    return $url;
+	return $url;
 }
 
 function assets_path( $type = null ) {
-    $url = WP_DIR . '/assets';
+	$url = WP_DIR . '/assets';
 
-    if ($type != null && in_array($type, array('css', 'favicons', 'fonts', 'img', 'js'))) {
-        $url .= '/' . $type;
-    }
+	if ($type != null && in_array($type, array('css', 'favicons', 'fonts', 'img', 'js'))) {
+		$url .= '/' . $type;
+	}
 
-    return $url;
+	return $url;
 }
 
 if ( defined( 'GOOGLE_ANALYTICS_ID' ) && ! empty( GOOGLE_ANALYTICS_ID ) ) {
@@ -41,35 +41,39 @@ function print_google_analytics_code() {
 
 add_action( 'wp_footer', 'print_svg_sprite', 0 );
 function print_svg_sprite() {
-	include WP_DIR . '/assets/img/svg-sprite.svg';
+	?>
+	<div style="display: none;">
+	<?php include WP_DIR . '/assets/img/svg-sprite.svg'; ?>
+	</div>
+	<?php
 }
 
 add_action( 'wp_footer', 'load_async_css', 1000 );
 function load_async_css() {
-    global $async_styles;
-    $libpath = WP_DIR . '/assets/js/lib/loadcss.min.js';
-    if ( is_array( $async_styles ) && count( $async_styles ) && file_exists( $libpath ) ) : ?>
-    <script type="text/javascript">
-        <?php
-        include $libpath;
-        foreach ( $async_styles as $style_src ) : ?>
-            loadCSS('<?= $style_src ?>');
-        <?php endforeach; ?>
-    </script>
-    <?php
-    endif;
+	global $async_styles;
+	$libpath = WP_DIR . '/assets/js/lib/loadcss.min.js';
+	if ( is_array( $async_styles ) && count( $async_styles ) && file_exists( $libpath ) ) : ?>
+	<script type="text/javascript">
+		<?php
+		include $libpath;
+		foreach ( $async_styles as $style_src ) : ?>
+			loadCSS('<?= $style_src ?>');
+		<?php endforeach; ?>
+	</script>
+	<?php
+	endif;
 }
 
 add_action( 'wp_footer', 'font_loader', 1000 );
 function font_loader() {
-    ?>
-    <script type="text/javascript">
-    <?php include WP_DIR . '/assets/js/lib/fontloader.min.js'; ?>
-    fontloader.onload({
-        'Lato': [400,300,700],
-    });
-    </script>
-    <?php
+	?>
+	<script type="text/javascript">
+	<?php include WP_DIR . '/assets/js/lib/fontloader.min.js'; ?>
+	fontloader.onload({
+		'Lato': [400,300,700],
+	});
+	</script>
+	<?php
 }
 
 add_action( 'wp_footer', 'browser_sync_snippet', 1000 );
@@ -89,11 +93,11 @@ function browser_sync_snippet() {
 		return;
 	}
 
-    ?>
-    <script id="__bs_script__">//<![CDATA[
-        document.write("<script async src='http://localhost:3000/browser-sync/browser-sync-client.js?v=<?= $browser_sync_json->version; ?>'><\/script>");
-    //]]></script>
-    <?php
+	?>
+	<script id="__bs_script__">//<![CDATA[
+		document.write("<script async src='http://localhost:3000/browser-sync/browser-sync-client.js?v=<?= $browser_sync_json->version; ?>'><\/script>");
+	//]]></script>
+	<?php
 }
 
 function critical_css_path() {
