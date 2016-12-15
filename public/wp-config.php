788caa7094eeade49af9a2f74c627de19098cd7f
Mirko Ferraro <mirkoferraro@gmail.com>
2016-12-15 22:27:46 +0100

    removed support for vagrant

--- public/wp-config.php
+++ public/wp-config.php
@@ -7,11 +7,8 @@ $webroot_dir = $root_dir . '/public';
 $server_name = isset ( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : '';
 $server_port = isset ( $_SERVER['SERVER_PORT'] ) ? $_SERVER['SERVER_PORT'] : '';
 
-// Support for Vagrant
-$is_vagrant = strpos( php_uname(), 'skeleton' ) !== false;
-
 // Project config
-$config_path = '/' . ( $is_vagrant ? 'vagrant-' : '' ) . 'local-config.php';
+$config_path = '/local-config.php';
 
 // Check for required files
 if ( ! file_exists( $root_dir . '/vendor/autoload.php' ) ) {
