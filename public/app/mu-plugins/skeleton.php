6d5cb7e45f1cdefbfba2ac441f04b810227ee2a4
Mirko Ferraro <mirkoferraro@gmail.com>
2017-01-19 10:02:42 +0100

    mu functions refactor

---
+++ public/app/mu-plugins/skeleton.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Skeleton Extensions
+Description:
+Version: 1.0.0
+Author: Mirko Ferraro
+Author URI: http://www.mirkoferraro.it
+*/
+
+check_directly_access();
+
+/*------------------------------------*\
+    Core functions
+\*------------------------------------*/
+function check_directly_access() {
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit;
+    }
+}
+
+function glob_recursive( $pattern, $flags = 0 ) {
+    $files = glob( $pattern, $flags );
+
+    foreach ( glob( dirname( $pattern ) . '/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir ) {
+        $files = array_merge( $files, glob_recursive( $dir . '/' . basename( $pattern ), $flags ) );
+    }
+
+    return $files;
+}
+
+function include_folders( $base_path, $folders = array() ) {
+
+    if ( is_array( $folders) && count( $folders) ) {
+
+        foreach ( $folders as $folder ) {
+
+            include_glob( $base_path . '/' . $folder );
+
+        }
+
+    } else {
+
+        include_glob( $base_path );
+
+    }
+
+}
+
+function include_glob( $path ) {
+
+    foreach ( glob( $path . '/*.php' ) as $file ) {
+        include_once( $file );
+    }
+
+    foreach ( glob_recursive( $path . '/*/*.php' ) as $file ) {
+        include_once( $file );
+    }
+
+}
+
+/*------------------------------------*\
+    Function modules
+\*------------------------------------*/
+include_folders( __DIR__ . '/skeleton', array(
+	'functions',
+	'custom',
+) );
--- public/app/themes/skeleton/functions.php
+++ public/app/themes/skeleton/functions.php
@@ -5,24 +5,12 @@ check_directly_access();
 /*------------------------------------*\
     Function modules
 \*------------------------------------*/
-$includes_path = get_template_directory() . '/includes';
-
-$folders = array(
+include_folders( get_template_directory() . '/includes', array(
 	'functions',
 	'post-types',
 	'custom-fields',
 	'custom',
-);
-
-foreach ( $folders as $folder ) {
-	foreach ( glob( $includes_path . '/' . $folder . '/*.php' ) as $function ) {
-	    include_once( $function );
-	}
-
-	foreach ( glob_recursive( $includes_path . '/' . $folder . '/*/*.php' ) as $function ) {
-	    include_once( $function );
-	}
-}
+) );
 
 
 /*------------------------------------*\
