e99a81e5170002302015560f8a1f6213b9d2bf53
Mirko Ferraro <mirkoferraro@gmail.com>
2017-01-20 10:10:22 +0100

    font face observer

--- public/app/mu-plugins/skeleton/functions/assets.php
+++ public/app/mu-plugins/skeleton/functions/assets.php
@@ -86,14 +86,116 @@ function load_async_css() {
 	endif;
 }
 
+function add_font_face_observer( $font_family, $font_weight ) {
+	global $font_face_observer;
+
+	$font_weight = (array) $font_weight;
+
+	$font_weight = array_map( 'strtolower', $font_weight );
+	$font_weight = array_filter( $font_weight, function( $weight ) {
+		return in_array( $weight, array(
+			'100',
+			'200',
+			'300',
+			'400',
+			'500',
+			'600',
+			'700',
+			'800',
+			'900',
+			'light',
+			'normal',
+			'medium',
+			'bold',
+			'extrabold',
+			'black',
+		) );
+	} );
+
+	foreach ( $font_weight as $weight ) {
+
+		switch( $weight ) {
+			case 'light':     $weight = '300'; break;
+			case 'normal':    $weight = '400'; break;
+			case 'medium':    $weight = '500'; break;
+			case 'bold':      $weight = '700'; break;
+			case 'extrabold': $weight = '800'; break;
+			case 'black':     $weight = '900'; break;
+		}
+
+		if ( ! isset( $font_face_observer ) || empty( $font_face_observer ) || ! is_array( $font_face_observer ) ) {
+			$font_face_observer = array();
+		}
+
+		if ( ! isset( $font_face_observer[$font_family] ) ) {
+			$font_face_observer[$font_family] = array();
+		}
+
+		if ( ! in_array( $weight, $font_face_observer[$font_family] ) ) {
+			$font_face_observer[$font_family][] = $weight;
+		}
+
+	}
+
+}
+
 add_action( 'wp_footer', 'font_loader', 1000 );
 function font_loader() {
+	global $font_face_observer;
+
+	if ( ! isset( $font_face_observer ) || ! count( $font_face_observer ) ) {
+		return;
+	}
+
 	?>
 	<script type="text/javascript">
-	<?php include assets_path('js') . '/lib/fontloader.min.js'; ?>
-	fontloader.onload({
-		'Lato': [400,300,700],
-	});
+	<?php include WP_DIR . '/../node_modules/fontfaceobserver/fontfaceobserver.standalone.js'; ?>
+	var
+	fonts = {
+		<?php foreach ( $font_face_observer as $fontfamily => $weights ) : ?>
+			<?php foreach ( $weights as $weight ) : ?>
+				<?= $fontfamily . $weight; ?>: new FontFaceObserver('<?= $fontfamily; ?>', { weight: <?= $weight; ?> }),
+			<?php endforeach; ?>
+		<?php endforeach; ?>
+	},
+	fonts_loaded = {
+		<?php foreach ( $font_face_observer as $fontfamily => $weights ) : ?>
+			<?php foreach ( $weights as $weight ) : ?>
+				<?= $fontfamily . $weight; ?>: false,
+			<?php endforeach; ?>
+		<?php endforeach; ?>
+	},
+	count = 0,
+	loaded = 0;
+
+	for (var name in fonts) {
+
+		if (typeof fonts[name].load !== 'function') {
+			continue;
+		}
+
+		count++;
+
+		(function(font, name) {
+
+			font.load(null, 3000).then(function () {
+				loaded++;
+				fonts_loaded[name] = true;
+				// document.documentElement.className += ' ' + name;
+
+				if (count == loaded) {
+					document.documentElement.className += ' fonts-loaded';
+				}
+			}, function() {
+				loaded++;
+
+				if (count == loaded) {
+					document.documentElement.className += ' fonts-loaded';
+				}
+			});
+
+		})(fonts[name], name);
+	}
 	</script>
 	<?php
 }
--- public/app/themes/skeleton/includes/functions/styles.php
+++ public/app/themes/skeleton/includes/functions/styles.php
@@ -4,6 +4,8 @@ check_directly_access();
 add_action( 'wp_enqueue_scripts', 'load_styles' );
 function load_styles() {
 
+    add_font_face_observer( 'Lato', array( 300, 400, 700 ) );
+    
     $path = assets_url( 'css' );
     $main_css_version = defined( 'MAIN_CSS_VERSION' ) ? '.' . MAIN_CSS_VERSION : '';
     wp_enqueue_async_style( 'main', $path . '/main.min' . $main_css_version . '.css' );
