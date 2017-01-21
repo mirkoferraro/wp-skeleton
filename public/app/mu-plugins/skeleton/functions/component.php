869e6ec428c4e3353fb58a775467f3939d7b1b2d
Mirko Ferraro <mirkoferraro@gmail.com>
2017-01-21 22:47:25 +0100

    custom template function

--- public/app/mu-plugins/skeleton/functions/component.php
+++ public/app/mu-plugins/skeleton/functions/component.php
@@ -1,48 +1,18 @@
 <?php
 
-function component( $name, $args = array(), $repeat = false ) {
+function component( $name, $args = array(), $repeat = false, $return = false ) {
 
-	if ( ! is_array( $args) ) {
-        throw new Exception( 'Invalid argument \'args\' in component function', 1 );
-	}
-
-    $name = apply_filters( 'filter_component_name', $name, $args );
-    $args = apply_filters( 'filter_component_args', $args, $name );
-    do_action( "get_component_{$name}", $name, $args );
-
-    $component_path = locate_template( array( "components/{$name}.php" ), false );
-
-	if ( $repeat ) {
-
-		foreach ( $args as $sub ) {
-
-			if ( count( $sub ) ) {
-				extract( $sub );
-			}
-
-			include $component_path;
-
-		}
-
-	} else {
-
-		if ( count( $args ) ) {
-			extract( $args );
-		}
-
-		include $component_path;
-
-	}
+	get_custom_template( 'component', 'components', $name, $args, $repeat, $return );
 
 }
 
 
-function components( $name, $args ) {
+function components( $name, $args, $return ) {
 
 	if ( ! is_array( $args) ) {
 		return;
 	}
 
-	component( $name, $args, true );
+	component( $name, $args, true, $return );
 
 }
--- public/app/mu-plugins/skeleton/functions/templates.php
+++ public/app/mu-plugins/skeleton/functions/templates.php
@@ -1,8 +1,8 @@
 <?php
 check_directly_access();
 
-add_filter( 'theme_page_templates' , 'custom_templates' );
-function custom_templates( $page_templates ) {
+add_filter( 'theme_page_templates' , 'load_theme_custom_templates' );
+function load_theme_custom_templates( $page_templates ) {
 
 	foreach ( glob( get_template_directory() . '/views/content-template-*.php' ) as $template_path ) {
 		$from          = strlen( get_template_directory() ) + 28;
@@ -15,4 +15,51 @@ function custom_templates( $page_templates ) {
 	}
 
 	return $page_templates;
+}
+
+function get_custom_template( $type, $path, $name, $args = array(), $repeat = false, $return = false ) {
+
+	if ( ! is_array( $args ) ) {
+        throw new Exception( 'Invalid argument \'args\' in template function', 1 );
+	}
+
+	$name = apply_filters( "filter_{$type}_name", $name, $args );
+    $args = apply_filters( "filter_{$type}_args", $args, $name );
+    do_action( "get_{$type}_{$name}", $name, $args );
+
+    $component_path = locate_template( array( "{$path}/{$name}.php" ), false );
+
+	if ( $return ) {
+		ob_start();
+	}
+
+	if ( $repeat ) {
+
+		foreach ( $args as $sub ) {
+
+			if ( count( $sub ) ) {
+				extract( $sub );
+			}
+
+			include $component_path;
+
+		}
+
+	} else {
+
+		if ( count( $args ) ) {
+			extract( $args );
+		}
+
+		include $component_path;
+
+	}
+
+	if ( $return ) {
+		$content = ob_get_contents();
+		ob_end_clean();
+
+		return $content;
+	}
+
 }
