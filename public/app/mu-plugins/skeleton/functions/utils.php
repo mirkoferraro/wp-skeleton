b87dc8f51c38181b3c4485e2b8ac48ba3c4088e0
Mirko Ferraro <mirkoferraro@gmail.com>
2017-02-07 22:50:49 +0100

    array_map_full function

--- public/app/mu-plugins/skeleton/functions/utils.php
+++ public/app/mu-plugins/skeleton/functions/utils.php
@@ -7,3 +7,17 @@ function array_map_keys( $closure, $array ) {
 		return $result;
 	}, array());
 }
+
+function array_map_full( $closure, $array, $map_type = ARRAY_A ) {
+	return array_reduce( $array, function ( $result, $item ) use ( $closure ) {
+		$map = $closure( $item );
+
+        if ( ARRAY_A == $map_type ) {
+            $result[ $map['key'] ] = $map['item'];
+        } elseif ( ARRAY_N == $map_type ) {
+            $result[ $map[0] ] = $map[1];
+        }
+
+		return $result;
+	}, array());
+}
