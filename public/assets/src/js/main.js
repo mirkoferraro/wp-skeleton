0a9becb85fe1604a2b6d8a2235d99b8d6c8d9384
Mirko Ferraro <mirkoferraro@gmail.com>
2017-02-27 13:49:52 +0100

    gulp + webpack + vue

--- public/assets/src/js/main.js
+++ public/assets/src/js/main.js
@@ -2,4 +2,4 @@ require('./lib/extend.js');
 require('./main/conditionizr.js');
 require('./main/event-wrapper.js');
 require('./main/gmaps.js');
-require('./vue/test.vue');
+require('./vue/vue.js');
---
+++ public/assets/src/js/vue/templates/block.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="block">
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  el: '#block',
+  data: {
+    title: 'Lorem ipsum',
+    text: 'Dolor sit amet'
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+#block {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  h1 {
+    font-weight: normal;
+  }
+}
+</style>
