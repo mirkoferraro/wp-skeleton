fba6063accd99133b39c86608ad27ac3528f2928
Mirko Ferraro <mirkoferraro@gmail.com>
2017-03-04 12:18:06 +0100

    requireAll (for view-js)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
+var requireAll = require('./lib/requireAll.js');
+
 require('./lib/extend.js');
 require('./main/conditionizr.js');
 require('./main/event-wrapper.js');
 require('./main/gmaps.js');
 require('./vue/vue.js');
+
+requireAll(require.context('../views/', true, /\.js$/));
