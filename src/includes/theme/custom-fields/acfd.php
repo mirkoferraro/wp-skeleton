ae3e4771ca85157fd8c032d4c587bc1253bbb617
Mirko Ferraro <mirkoferraro@gmail.com>
2017-03-06 23:08:57 +0100

    acf header / integrations

---
+++ src/includes/theme/custom-fields/acfd.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( class_exists( 'ACFD' ) && ACFD::isActive() ) {
+	
+	$group = new CustomGroup('Header', 'options_page == acf-options-Header');
+	$group->addField('logo', 'Logo', 'image');
+	
+	$group = new CustomGroup('Integrations', 'options_page == acf-options-Integrations');
+	$group->addField('google_analytics_id', 'Google Analytics ID', 'text');
+	$group->addField('google_tag_manager_id', 'Google Tag Manager ID', 'text');
+	$group->addField('google_api_key', 'Google API Key', 'text');
+
+}
