7b5c5d4d9dbc43ae8ae147b51e63247d2e85d5f7
Mirko Ferraro <mirkoferraro@gmail.com>
2017-04-08 17:00:08 +0200

    fix 404

--- src/views/content-404.php
+++ src/views/content-404.php
@@ -2,7 +2,7 @@
     <article>
         <h1><?= _t( 'Page not found' ); ?></h1>
         <h2>
-            <a href="<?= $home_url; ?>"><?= _t( 'Return home?' ); ?></a>
+            <a href="<?= home_url() ?>"><?= _t( 'Return home?' ); ?></a>
         </h2>
     </article>
 </section>
