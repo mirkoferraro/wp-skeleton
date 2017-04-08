7ca59086aead53a43c27cf49d80cbb92e2ed047a
Mirko Ferraro <mirkoferraro@gmail.com>
2017-04-08 18:48:30 +0200

    wordpress core alert

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
+$loader = __DIR__ . '/core/wp-blog-header.php';
+
+if ( ! file_exists( $loader ) ) {
+    die( 'WordPress is not installed. Use <i>wp core download</i> command' );
+}
+
 define( 'WP_USE_THEMES', true );
-require( __DIR__ . '/core/wp-blog-header.php' );
+require( $loader );
