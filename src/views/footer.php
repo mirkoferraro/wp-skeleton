af835dca139c46d7a31bf91c2f4ba410a75010e3
Mirko Ferraro <mirkoferraro@gmail.com>
2017-04-20 12:04:45 +0200

    update footer link

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer column" role="contentinfo">
 	<p class="copyright">
-		<span><?= _t('Powered by'); ?> </span> <a href="//skeleton.com" title="Skeleton">Skeleton</a>.
+		<span><?= _t('Powered by'); ?> </span> <a href="https://github.com/mirkoferraro/wp-skeleton" title="Skeleton">Skeleton</a>.
 	</p>
 </footer>
 
